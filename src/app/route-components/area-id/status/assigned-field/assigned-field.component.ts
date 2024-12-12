e78ec1a08e47b564e4b1aa28061c6bf964a5af0b
Jose Lito Phala <joselitophala@gmail.com>
2024-12-12 12:42:10 +0000

    converted rxjs api call to rxresource

--- src/app/route-components/area-id/status/assigned-field/assigned-field.component.ts
+++ src/app/route-components/area-id/status/assigned-field/assigned-field.component.ts
@@ -1,8 +1,9 @@
 import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { AreaIdStateService } from '../../area-id-state.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { AssignedFieldService } from './assigned-field.service';
+import { consumerPollProducersForChange } from '@angular/core/primitives/signals';
 
 @Component({
   selector: 'app-assigned-field',
@@ -10,13 +11,17 @@ import { AssignedFieldService } from './assigned-field.service';
   templateUrl: './assigned-field.component.html',
   styles: ``
 })
-export class AssignedFieldComponent {
+export class AssignedFieldComponent implements OnInit {
 
     private areaIdStateService = inject(AreaIdStateService);
     private assignedFieldService = inject(AssignedFieldService);
 
     protected isOptionOpen = signal<boolean>(true);
-    protected users = toSignal(this.assignedFieldService.getUsersByArea(this.areaIdStateService.areaId), {initialValue: {success: false, data: []}});
+    protected users = this.assignedFieldService.userList;
+
+    ngOnInit(): void {
+        this.assignedFieldService.setAreaId(this.areaIdStateService.areaId);
+    }
 
     toggleOverlayHandler(){
         this.isOptionOpen.set(!this.isOptionOpen());
--- src/app/route-components/area-id/status/assigned-field/assigned-field.service.ts
+++ src/app/route-components/area-id/status/assigned-field/assigned-field.service.ts
@@ -1,7 +1,10 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { AreaUserAPIResponseType } from '../../../../generic';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { tap } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +12,17 @@ import { AreaUserAPIResponseType } from '../../../../generic';
 export class AssignedFieldService {
 
     private http = inject(HttpClient);
+    private areaId = signal<number | null>(null);
+    userList = rxResource({
+        request: () => ({id: this.areaId()}),
+        loader: ({request}) =>  this.http.get<AreaUserAPIResponseType>(environment.backend_api_url + environment.backend_api.area_user.get + `?area_id=${request.id}`)})
 
     getUsersByArea(areaId: number){
         return this.http.get<AreaUserAPIResponseType>(environment.backend_api_url + environment.backend_api.area_user.get + `?area_id=${areaId}`);
+    }
+
+    setAreaId(areaId: number){
+        this.areaId.set(areaId);
     }
   
 }
